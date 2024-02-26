d0b8680f45c0596c7cd0e5e865006ed5606a8e99
Kasthuri Shekar <Kasthuri.Shekar@dowjones.com>
2024-02-26 16:56:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import './style.css';
 
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://randomuser.me/api/');
+      const { results: [user] } = response.data; 
+      const { name: { title, first, last }, email } = user;
+      setUserData({ fullName: `${title} ${first} ${last}`, email });
+      localStorage.setItem('userData', JSON.stringify(userData)); 
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("userData");
+    if (storedData) {
+      setUserData(JSON.parse(storedData));
+    } else {
+      fetchData();
+    }
+  }, []);
+
+  const handleRefresh = () => {
+    setUserData(null);
+    fetchData();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="user-container">
+      {userData ? (
+        <React.Fragment>
+          <h2 className="user-name">Full Name: {userData.fullName}</h2>
+          <p className="user-email">Email: {userData.email}</p>
+        </React.Fragment>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <button className="refresh-btn" onClick={handleRefresh}>Refresh User</button>
     </div>
   );
 }
